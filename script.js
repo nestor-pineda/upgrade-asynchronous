9be83eaa68cb1ba69674eab25bcf50344bdac477
Nestor Pineda <nestorpinedaelvira@gmail.com>
2021-11-05 19:57:37 +0100

    Iteration #2

--- script.js
+++ script.js
@@ -41,3 +41,39 @@ const myButton = document.querySelector("button").addEventListener("click", () =
 
   insertButton(); */
 });
+
+// #2: async-await
+// 2.1 Convierte la siguiente promesa para esperar a ejecutar el console usando async-await.
+
+const runTimeOut = () => {
+  const promise = new Promise((resolve) => {
+    setTimeout(function () {
+      resolve();
+    }, 2000);
+  });
+
+  promise.then(() => {
+    console.log("Time out!");
+  });
+  /* let promise = async () => {
+    
+  } */
+};
+
+// 2.2 Convierte la siguiente función con un fetch utilizando async-await. Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador;
+/* 
+function getCharacters() {
+  fetch("https://rickandmortyapi.com/api/character")
+    .then((res) => res.json())
+    .then((characters) => console.log(characters));
+}
+
+getCharacters(); */
+
+const getCharacters = async () => {
+  const res = await fetch("https://rickandmortyapi.com/api/character");
+  const data = await res.json();
+  console.log(data.characters);
+};
+
+getCharacters();
